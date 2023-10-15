2f0c8f5678152fd67c88abf30a6b42d33f533a29
Peter <peterdecon@gmail.com>
2023-10-15 16:25:05 +0200

    More Features

--- revealPresenter/kwismaster.js
+++ revealPresenter/kwismaster.js
@@ -1,16 +1,31 @@
 function makeQuestionSlides(roundObj, questionObj) {
+
     var roundTitleDOM = document.createElement('section');
     roundTitleDOM.innerHTML = roundObj["name"];
 
+    if (Object.keys(settings.specs).includes(roundObj["name"])) {
+        console.log("found a setting for " + roundObj["name"]);
+        if (settings.specs[roundObj["name"]].includes("NO_QUESTIONS_ONLY")) {
+            console.log("Skipping questions slides for this round")
+            return;
+        }
+
+    } else {
+        console.log("found no setting for " + roundObj["name"]);
+    }
+
     slides.appendChild(roundTitleDOM);
     for (question in questionObj) {
+        var imgRatio = 1;
         var questionDOM = document.createElement('section');
 
         var questionTitle = document.createElement('h4');
         var questionCategory = document.createElement('h5');
         var questionCounter = Number(question) + 1;
         questionTitle.innerHTML = roundObj["name"] + " - " + "Vraag " + questionCounter;
-        questionCategory.innerHTML = questionObj[question]["category"];
+        if ("category" in questionObj[question]) {
+            questionCategory.innerHTML = questionObj[question]["category"];
+        }
 
         var questionBody = document.createElement('section');
 
@@ -32,10 +47,11 @@ function makeQuestionSlides(roundObj, questionObj) {
         questionDOM.appendChild(questionBody);
 
         if (questionObj[question]["img"]) {
-            console.log("Show the image (" + roundObj["name"] + "){" + questionObj[question]["img"] + "}")
+            console.log("restyling");
             var questionImg = document.createElement('img');
-            questionImg.src = "images/" + questionObj[question]["img"];
-            questionImg.style.height = '225px';
+            questionImg.src = "images/" + questionObj[question]["img"].split(":")[0];
+            imgRatio = questionObj[question]["img"].split(":")[1] || 1;
+            questionImg.style.height = (300 * imgRatio) + 'px';
             questionBody.appendChild(questionImg);
         }
 
@@ -50,9 +66,8 @@ function makeAnswerSlides(roundObj, questionObj, excludeQuestions = []) {
 
     slides.appendChild(roundTitleDOM);
     for (question in questionObj) {
-        console.log(questionObj[question]["category"], excludeQuestions);
+        var imgRatio = 1;
         if (excludeQuestions.includes(questionObj[question]["category"])) {
-            console.log("Excluding");
             continue;
         }
         var questionDOM = document.createElement('section');
@@ -61,20 +76,24 @@ function makeAnswerSlides(roundObj, questionObj, excludeQuestions = []) {
         var questionCategory = document.createElement('div');
         var questionCounter = Number(question) + 1;
         questionTitle.innerHTML = roundObj["name"] + " - " + "Vraag " + questionCounter;
-        questionCategory.innerHTML = questionObj[question]["category"];
+        if ("category" in questionObj[question]) {
+            questionCategory.innerHTML = questionObj[question]["category"];
+        }
 
         var questionBody = document.createElement('section');
         var questionBodyQuestion = document.createElement('div');
         var questionBodyAnswer = document.createElement('div');
 
         questionBodyQuestion.innerHTML = questionObj[question]["shortQuestion"];
-        questionBodyAnswer.innerHTML = "[" + questionObj[question]["answer"] + "]";
+        if (questionObj[question]["answer"] !== "") {
+            questionBodyAnswer.innerHTML = "[" + questionObj[question]["answer"] + "]";
+        }
         questionBody.appendChild(questionBodyQuestion);
         if (questionObj[question]["img"]) {
-            console.log("Show the image (" + roundObj["name"] + "){" + questionObj[question]["img"] + "}")
             var questionImg = document.createElement('img');
-            questionImg.src = "images/" + questionObj[question]["img"];
-            questionImg.style.height = '225px';
+            questionImg.src = "images/" + questionObj[question]["img"].split(":")[0];
+            imgRatio = questionObj[question]["img"].split(":")[1] || 1;
+            questionImg.style.height = (300 * imgRatio) + 'px';
             questionBody.appendChild(questionImg);
         }
         questionBody.appendChild(questionBodyAnswer);
@@ -88,55 +107,24 @@ function makeAnswerSlides(roundObj, questionObj, excludeQuestions = []) {
 }
 
 function domQuestions() {
-    var slides = Reveal.getSlidesElement();
 
     for (var round in questions) {
         var roundObj = questions[round];
+        var pat = /CATEGORY:/g;
+        // if (Object.keys(settings.specs).includes(roundObj["name"])) {
+        //     console.log(settings.specs[roundObj["name"]].some(e => pat.test(e)));
+        //     console.log("Need to extract category from all questions")
+        // } else {
         var questionObj = roundObj["questions"];
 
         makeQuestionSlides(roundObj, questionObj);
-        makeAnswerSlides(roundObj, questionObj, ["Rode Draad"]);
+        //hardcoded for traddans, needs to change
+        if (roundObj["name"] === "Rode Draad") {
+            makeAnswerSlides(roundObj, questionObj);
+        } else {
+            makeAnswerSlides(roundObj, questionObj, ["Rode Draad"]);
+        }
     }
-
-    // for (var round in questions) {
-    //     var questionRound = questions[round];
-
-
-    //     var title = document.createElement('div');
-    //     title.innerHTML = questionRound.name;
-
-    //     var titleOnly = document.createElement('div');
-    //     titleOnly.innerHTML = questionRound.name;
-
-
-    //     var roundSectionTitleOnly = document.createElement("section");
-    //     roundSectionTitleOnly.appendChild(titleOnly);
-
-    //     var roundSection = document.createElement("section");
-    //     roundSection.appendChild(title);
-
-    //     slides.appendChild(roundSectionTitleOnly);
-
-
-
-
-    //     for (var question in questionRound["questions"]) {
-    //         var subtitle = document.createElement('div');
-    //         var questionQuestion = questionRound["questions"][question];
-    //         var questionSection = document.createElement("section");
-
-    //         var questionDiv = document.createElement("div");
-    //         questionDiv.innerHTML = questionQuestion["shortQuestion"];
-    //         questionSection.appendChild(subtitle);
-    //         questionSection.appendChild(questionDiv);
-
-    //         roundSection.appendChild(questionSection);
-
-    //         slides.appendChild(roundSection);
-    //     }
-
-
-    // }
 }
 
 window.addEventListener("load", (event) => {
