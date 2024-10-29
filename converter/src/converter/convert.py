244d660898df2a90077b7e828c1abfe4477c450e
Peter <peterdecon@gmail.com>
2024-10-29 20:25:24 +0100

    [BREAKING] extract code for input-file to Question obj to question_parser.py and related test

--- converter/src/converter/convert.py
+++ converter/src/converter/convert.py
@@ -1,7 +1,9 @@
 import json
 import sys, os
-from translations import translations
+
 from shutil import copy2, rmtree
+
+from question_parser import getAllQuestionsFromFiles
 
 import shutil
 def copy_tree(src, dst, symlinks=False, ignore=None):
@@ -20,112 +22,11 @@ class MyEncoder(json.JSONEncoder):
 	def default(self, o):
 		return o.__dict__
 
-class Question:
-	def __init__(self):
-		self.longQuestion = ""
-		self.shortQuestion = ""
-		self.answer = ""
-		self.category = ""
-		self.sound = ""
-		
-	def setLongQuestion(self, txt):
-		self.longQuestion += txt.strip()
-		self.lastUsed = self.setLongQuestion
-
-	def setShortQuestion(self, txt):
-		self.shortQuestion += txt.strip()
-		self.lastUsed = self.setShortQuestion
-		
-	def setAnswer(self, txt):
-		self.answer += txt.strip()
-		self.lastUsed = self.setAnswer
-		
-	def setImg(self, txt):
-		self.img = txt.strip()
-		
-	def setCategory(self, txt):
-		self.category = txt.strip()
-		
-	def setRound(self, txt):
-		self.round = txt.strip()
-
-	def setSound(self, filename):
-		self.sound = filename.strip()
-
-	def setSourceFile(self, txt):
-		self.sourceFile = txt
-
-	def appendToLastUsedField(self, txt):
-		self.lastUsed(r"\n" + txt.strip())
-
-	def __repr__(self) -> str:
-		return self.round + " - " + self.category + " - " + self.shortQuestion 
-
-def detectLanguage(line):
-	if line.lower().startswith("lange vraag"):
-		return "NL"
-	elif line.lower().startswith("long question"):
-		return "EN"
-	else:
-		return None
-
-#parse the user friendly syntax to Question objects
-def readSingleFile(filename):
-	fileQuestions = []
-	detectedLanguage = None
-
-	
-	print("Reading " + filename)
-	currentQuestion = None
-	with open(filename ,'r', encoding='utf-8') as questionFile:		
-		for line in questionFile:
-			if not ':' in line:
-				if currentQuestion and line != "\n":
-					currentQuestion.appendToLastUsedField(line)
-				continue
-
-			key, value = line.split(':',1)
-			if value.strip() == "/" or value.strip() == "\\":
-				print("Skipping line " + line + " because content is single forward or backward slash")
-				continue
-
-			#lange vraag, or its translation, should be the first line in a block, if you encounter it, finish the current question
-			if not detectedLanguage:
-				detectedLanguage = detectLanguage(line)
-				print(f"detected {detectedLanguage} as the language")
-			if key.lower().strip() == translations[detectedLanguage]["longQuestion"].lower().strip():
-				if(currentQuestion):
-					currentQuestion.setSourceFile(filename)
-					fileQuestions.append(currentQuestion)				
-				currentQuestion = Question()
-				currentQuestion.setLongQuestion(value)
-			elif key.lower().strip() == translations[detectedLanguage]["shortQuestion"].lower().strip():
-				currentQuestion.setShortQuestion(value)
-			elif key.lower().strip() == translations[detectedLanguage]["answer"].lower().strip():
-				currentQuestion.setAnswer(value)
-			elif key.lower().strip() == translations[detectedLanguage]["image"].lower().strip():
-				currentQuestion.setImg(value)
-			elif key.lower().strip() == translations[detectedLanguage]["category"].lower().strip():
-				currentQuestion.setCategory(value)
-			elif key.lower().strip() == translations[detectedLanguage]["round"].lower().strip():
-				currentQuestion.setRound(value)
-			elif key.lower().strip() == translations[detectedLanguage]["sound"].lower().strip():
-				currentQuestion.setSound(value)
-			else:
-				print(f"Skipping line {line}")
-
-	# no more "lange vraag" line means you have to write the current ongoing question
-	if currentQuestion:
-		currentQuestion.setSourceFile(filename)
-		fileQuestions.append(currentQuestion)
-
-	return fileQuestions
-
 def getAllQuestionsOrderedByRoundSpec(unhandledQuestions):
 	import re
 	import copy
-	#loop over the rounds, find if there is a round that has a CATEGORY: spec, and create a new round containing these questions
-	print(unhandledQuestions)
+
+	# Sort questions based on the round they appear in, using the rounds list as a reference
 	unhandledQuestions.sort(key=lambda question: [round.lower() for round in rounds].index(question.round.lower()))
 	questionsToAppend = []
 	for round in rounds:
@@ -147,11 +48,6 @@ def getAllQuestionsOrderedByRoundSpec(unhandledQuestions):
 	unhandledQuestions.sort(key=lambda question: [round.lower() for round in rounds].index(question.round.lower()))
 	return unhandledQuestions
 
-def getAllQuestionsFromFiles(inFiles):
-	allQuestions = []
-	for fileName in inFiles:
-		allQuestions += readSingleFile(fileName)
-	return allQuestions
 
 usageText = """
 USAGE:
@@ -210,7 +106,15 @@ def enablePresenter(title):
 		
 	with open("kwismaster.html", "w") as w:
 		w.write(kwismaster)
-	
+
+
+def detectLanguage(line):
+	if line.lower().startswith("lange vraag"):
+		return "NL"
+	elif line.lower().startswith("long question"):
+		return "EN"
+	else:
+		return None
 
 
 if __name__ == '__main__':
@@ -251,7 +155,12 @@ if __name__ == '__main__':
 	if "specs" in config:
 		specifications = config["specs"]
 
-	allQuestions = getAllQuestionsFromFiles([projectDir + "/" + infile for infile in infiles])
+	allInputFiles = [projectDir + "/" + infile for infile in infiles]
+	with open(allInputFiles[0]) as f:
+		first_line = f.readline().strip('\n')
+	language = detectLanguage(first_line)
+
+	allQuestions = getAllQuestionsFromFiles(allInputFiles, language)
 	questionSortedByRound = getAllQuestionsOrderedByRoundSpec(allQuestions)
 
 	questions = []
--- converter/tests/test_generalFunctionality.py
+++ converter/tests/test_generalFunctionality.py
@@ -1,69 +1,17 @@
-from src.converter.convert import readSingleFile, getAllQuestionsFromFiles
+from src.converter.convert import detectLanguage
 
-def test_singleQuestion():
-	foundQuestions = readSingleFile("tests/resources/SingleMinimalQuestion_NL.txt")
-	assert len(foundQuestions) == 1
-	
-	question = foundQuestions[0]
-	assert question.longQuestion == "This is the long question?"
-	assert question.shortQuestion == "This is the short question?"
-	assert question.answer == "This is the answer"
-	assert question.category == "This is the category"
-	assert question.round == "This is the round"
+def test_ifLineStartsWithLangeVraag_ThenLanguageIsDutch():
+    assert detectLanguage("Lange Vraag") == "NL"
+    assert detectLanguage("LANGE VRAAG") == "NL"
+    assert detectLanguage("lange vraag") == "NL"
+    assert detectLanguage("lange VRAAG") == "NL"
+    assert detectLanguage("LANGE vraag") == "NL"
+    assert detectLanguage("LANGE vraag: and then some useless data") == "NL"
 
-def test_singleQuestion_EN():
-	foundQuestions = readSingleFile("tests/resources/SingleMinimalQuestion_EN.txt")
-	assert len(foundQuestions) == 1
-	
-	question = foundQuestions[0]
-	assert question.longQuestion == "This is the long question?"
-	assert question.shortQuestion == "This is the short question?"
-	assert question.answer == "This is the answer"
-	assert question.category == "This is the category"
-	assert question.round == "This is the round"
-	
-def test_multipleQuestions_oneFile():
-	foundQuestions = readSingleFile("tests/resources/MultipleMinimalQuestions_NL.txt")
-	assert len(foundQuestions) == 2
-	
-	question = foundQuestions[0]
-	assert question.longQuestion == "This is the long question?"
-	assert question.shortQuestion == "This is the short question?"
-	assert question.answer == "This is the answer"
-	assert question.category == "This is the category"
-	assert question.round == "This is the round"
-	
-	question = foundQuestions[1]
-	assert question.longQuestion == "This is the long question 2?"
-	assert question.shortQuestion == "This is the short question 2?"
-	assert question.answer == "This is the answer 2"
-	assert question.category == "This is the category 2"
-	assert question.round == "This is the round 2"
-
-def test_multipleQuestions_twoInputFiles():
-	allQuestions = getAllQuestionsFromFiles(["tests/resources/SingleMinimalQuestion_NL.txt","tests/resources/SingleQuestion_withNewlines_NL.txt"])
-	assert len(allQuestions) == 2
-
-def test_multipleQuestions_twoInputFiles_multiLingual():
-	allQuestions = getAllQuestionsFromFiles(["tests/resources/SingleMinimalQuestion_NL.txt","tests/resources/SingleMinimalQuestion_EN.txt"])
-	assert len(allQuestions) == 2
-
-#maintain newlines in strings
-def test_lineWithoutPrefixShouldBeAddedToPreviousObject():
-	foundQuestions = readSingleFile("tests/resources/SingleQuestion_withNewlines_NL.txt")
-	assert len(foundQuestions) == 1
-	
-	question = foundQuestions[0]
-	assert question.longQuestion == r"This is the long question?\nIt contains a new line"
-	assert question.shortQuestion == r"This is the short question?\nwhich also contains a new line"
-	assert question.answer == r"This is the answer\nnew line is allowed here as well"
-	assert question.category == "This is the category"
-	assert question.round == "This is the round"
-
-#maintain newlines in strings
-def test_lineWithoutPrefixShouldBeAddedToPreviousObject():
-	foundQuestions = readSingleFile("tests/resources/SingleQuestion_withMusicFile.txt")
-	assert len(foundQuestions) == 1
-	
-	question = foundQuestions[0]
-	assert question.sound == "testRecording.mp3"
+def test_ifLineStartsWithLongQuestion_ThenLanguageIsDutch():
+    assert detectLanguage("Long Question") == "EN"
+    assert detectLanguage("LONG QUESTION") == "EN"
+    assert detectLanguage("long question") == "EN"
+    assert detectLanguage("LONG question") == "EN"
+    assert detectLanguage("long QUESTION") == "EN"
+    assert detectLanguage("Long Question: and then some useless data") == "EN"
